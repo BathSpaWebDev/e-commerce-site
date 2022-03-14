3168a04021142f8dd8dff6cb0d961dc34af3d361
Lee J Allen <leeallen737@gmail.com>
2022-03-14 13:58:03 +0000

    react app clean up, added sass and component folder

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lee</h1>
+      <h1>Sarah</h1>
+      <h1>Mohsin</h1>
+    </div>
+  );
+}
+
+export default App;
