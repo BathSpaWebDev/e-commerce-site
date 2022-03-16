e464a4d38d5e0928784f3c39777af62732cde3c0
Mohsin Sarker <mohsin.312007@gmail.com>
2022-03-16 11:13:03 +0000

    Added new navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.scss';
 import chairData from './chair_images.json';
 import shoeData from './shoe_images.json';
 import ItemContainer from './component/ItemSmall';
+import {NavBar} from './component/NavBar';
 
 function App() {
   console.log(shoeData.results)
   console.log(chairData.results);
   return (
     <div className="App">
+      <NavBar/>
       <ItemContainer/>
     </div>
   );
