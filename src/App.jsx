752582bcf9903a91d357ec76d8b81d5a3d052560
Lee Allen <leeallen737@gmail.com>
2022-03-17 09:13:31 +0000

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import './App.scss';
 import chairData from './chair_images.json';
 import shoeData from './shoe_images.json';
 import ItemContainer from './component/ItemContainer';
+import { NavBar } from './component/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <ItemContainer chairData={chairData} shoeData={shoeData}/>
     </div>
   );
---
+++ src/component/NavBar.jsx
@@ -0,0 +1,11 @@
+import './NavBar.scss';
+
+export const NavBar = () => {
+    return (
+        <div className='nav-container'>
+            <nav className='nav-list'>
+                <a>Cart</a>
+            </nav>
+        </div>
+    )
+}
