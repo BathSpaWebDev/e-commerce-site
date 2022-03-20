ac334983a9bb630f18a0468edd821f3f635b66d8
Mohsin Sarker <mohsin.312007@gmail.com>
2022-03-20 14:49:31 +0000

    Added a pagination component

--- src/component/ItemContainer.jsx
+++ src/component/ItemContainer.jsx
@@ -1,16 +1,32 @@
+import { useState } from "react";
 import "../scss/component/ItemContainer.scss";
 import { ItemSmall } from "./ItemSmall";
+import { Pagination } from "./Pagination"
 
 function ItemContainer({ chairData, getIndex, reveilItem, shoeData }) {
+
+  const [page, setPage] = useState(0);
+  const showPerPage = 6;
+  const pageVisited = page * showPerPage;
+  const totalData = chairData.results.length;
+  const handleChange = ({selected}) => {
+    console.log(selected);
+    setPage(selected);
+}
+
+
   return (
     <>
-      {chairData.results.map((item, i) => {
+      {chairData.results
+      .slice(pageVisited, pageVisited + showPerPage)
+      .map((item, i) => {
         return (
-          <div className="item-container">
+          <div className="item-container" key={i}>
             <ItemSmall reveilItem={reveilItem} index={i} getIndex={getIndex} key={item} itemData={item} />
           </div>
         );
       })}
+      <Pagination show={showPerPage} totalData={totalData} handleChange={handleChange}/>
     </>
   );
 }
